e7e030a726942ba17d0fd8b3f170bd2cc936efbb
Mattias Aronsson <aronsson.mattias@gmail.com>
2016-01-24 00:20:43 +0100

    Changed falling from physics to math

--- Assets/Scripts/OnTrigger.cs
+++ Assets/Scripts/OnTrigger.cs
@@ -3,23 +3,22 @@ using System.Collections;
 
 public class OnTrigger : MonoBehaviour {
 
-    void OnCollisionEnter()
+    void OnCollisionEnter(Collision collision)
     {
-        float hoverForce = 1.0f;
-        Debug.Log("Trigger enter");
-        this.GetComponent<Rigidbody>().isKinematic = false;
-        this.GetComponent<Rigidbody>().useGravity = true;
-        Physics.gravity = new Vector3(0, -0.7f, 0);
-        //GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        Debug.Log("Collision enter");
 
         Renderer rend = transform.Find("inverted_cube").GetComponent<Renderer>();
         rend.enabled = true;
+        rend.material.color = Vector4.zero;
         transform.Find("inverted_cube").GetComponent<Fade>().StartFade();
 
         AudioSource audio = GetComponent<AudioSource>();
         audio.Play();
 
         RigMover.instance.SetPlatform(null);
+
+        Transform rig = transform.parent.transform;
+        rig.GetComponent<Fall>().StartFalling();
 
     }
 }
