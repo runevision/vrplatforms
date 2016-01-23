f99455b114017ba9022f8234452186ad1c7d4b3c
Rune Skovbo Johansen <rune@unity3d.com>
2016-01-23 13:45:07 +0100

    First iteration of camera rig mover script.

--- Assets/Scripts/RigMover.cs
+++ Assets/Scripts/RigMover.cs
@@ -3,13 +3,52 @@ using System.Collections;
 
 public class RigMover : MonoBehaviour {
 
+	public static RigMover instance;
+
+	public Transform rig;
+	public float transitionSpeed = 1;
+
+	private float transitionDuration = 0.01f;
+
+	private Platform oldPlatform;
+	private Platform newPlatform;
+	private float switchStartTime = 0;
+
 	// Use this for initialization
-	void Start () {
-	
+	void OnEnable () {
+		instance = this;
 	}
-	
+
+	public void SetPlatform (Platform platform) {
+		oldPlatform = newPlatform;
+		newPlatform = platform;
+		switchStartTime = Time.time;
+
+		if (oldPlatform == null) {
+			transitionDuration = 0;
+			return;
+		}
+
+		Vector3 oldPos = GetRigPositionFromPlatform (oldPlatform);
+		Vector3 newPos = GetRigPositionFromPlatform (newPlatform);
+		transitionDuration = Vector3.Distance (oldPos, newPos);
+		transitionDuration = Mathf.Max (transitionDuration, 0.01f);
+	}
+
 	// Update is called once per frame
-	void Update () {
-	
+	void LateUpdate () {
+		float lerp = Mathf.Clamp01 ((Time.time - switchStartTime) / transitionDuration);
+
+		if (lerp == 1 || oldPlatform == null)
+			rig.transform.position = GetRigPositionFromPlatform (newPlatform);
+
+		Vector3 oldPos = GetRigPositionFromPlatform (oldPlatform);
+		Vector3 newPos = GetRigPositionFromPlatform (newPlatform);
+		rig.transform.position = Vector3.Lerp (oldPos, newPos, lerp);
+	}
+
+	Vector3 GetRigPositionFromPlatform (Platform platform) {
+		return platform.platform.position
+			+ new Vector3 (platform.tileX, 0, platform.tileY) * Controller.instance.size;
 	}
 }
