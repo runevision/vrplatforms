c248db47df7e73b7546b4211621aa5cae3ca877f
Rune Skovbo Johansen <rune@unity3d.com>
2016-01-23 12:14:04 +0100

    Update editor UI.

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -7,11 +7,19 @@ public class Controller : MonoBehaviour {
 	static Controller m_Instance;
 	public static Controller instance { get { return m_Instance; } }
 
+	[Range (4, 32)]
 	public float duration = 4;
+	[Range (0.5f, 1.5f)]
 	public float size = 1;
 
 	// Use this for initialization
 	void Awake () {
 		m_Instance = this;
 	}
+
+	#if UNITY_EDITOR
+	void OnValidate () {
+		UnityEditor.SceneView.RepaintAll ();
+	}
+	#endif
 }
