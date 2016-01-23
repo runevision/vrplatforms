21bd73dcfa0b9fbf79cb29e75656e9ee531e7edd
Mattias Aronsson <aronsson.mattias@gmail.com>
2016-01-23 15:54:32 +0100

    Moving plattforms now have pause time at each end. Duration is for A->B not A->B->A Custom duration for platforms. Move only once in but not tested.

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -7,10 +7,12 @@ public class Controller : MonoBehaviour {
 	static Controller m_Instance;
 	public static Controller instance { get { return m_Instance; } }
 
-	[Range (4, 32)]
-	public float duration = 4;
+    [Range(2, 16)]
+    public float duration = 2;
 	[Range (0.5f, 1.5f)]
-	public float size = 1;
+    public float size = 1;
+    [Range(1, 15)]
+    public float pause = 1;
 
 	// Use this for initialization
 	void OnEnable () {
--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -13,26 +13,81 @@ public class Platform : MonoBehaviour {
 	public Transform endB;
 	public Transform platform;
 
+    public bool customDuration;
+    [Range(2, 16)]
+    public float duration = 2;
+    public bool onlyOnce;
+
 	public Material markerMaterialA;
 	public Material markerMaterialB;
+
+    private float timer = 0;
+    private float adjust = 0;
+    private bool start = false;
 
 	// Use this for initialization
 	void Start () {
 		if (Application.isPlaying)
 			platform.gameObject.SetActive (true);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (!platform.gameObject.activeInHierarchy)
-			return;
-		
-		float lerp = Mathf.Sin (Time.time * Mathf.PI * 2 / Controller.instance.duration) * 0.5f + 0.5f;
-		platform.position = Vector3.Lerp (endA.position, endB.position, lerp);
 
-		if (Controller.instance.size != platform.localScale.x)
-			platform.localScale = Vector3.one * Controller.instance.size;
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        float localDuration;
+        if (customDuration)
+            localDuration = duration;
+        else
+            localDuration = Controller.instance.duration;
+
+        if (!platform.gameObject.activeInHierarchy)
+            return;
+
+        if (onlyOnce)
+        {
+            if (start)
+            {
+                if (timer == 0)
+                    timer = Time.time;
+
+                float lerp;
+                if (Time.time - timer + 0.1 < localDuration)
+                {
+                    lerp = Mathf.Cos((Time.time - adjust) * Mathf.PI * 2 / (localDuration * 2)) * 0.5f + 0.5f;
+                }
+                else
+                {
+                    lerp = 1;
+                }
+
+                platform.position = Vector3.Lerp(endA.position, endB.position, lerp);
+            }
+        }
+        else
+        {
+            if (timer == 0)
+                timer = Time.time;
+
+            float lerp;
+            if (Time.time - timer + 0.1 < localDuration)
+            {
+                lerp = Mathf.Cos((Time.time - adjust) * Mathf.PI * 2 / (localDuration * 2)) * 0.5f + 0.5f;
+            }
+            else
+            {
+                lerp = (adjust / Controller.instance.pause) % 2; //Should be zero or one depening on where in time we are.
+                if (Time.time - timer > localDuration + Controller.instance.pause)
+                {
+                    timer = Time.time;
+                    adjust += Controller.instance.pause;
+                }
+            }
+            platform.position = Vector3.Lerp(endA.position, endB.position, lerp);
+        }
+
+        if (Controller.instance.size != platform.localScale.x)
+            platform.localScale = Vector3.one * Controller.instance.size;
+    }
 
 	void OnRenderObject () {
 		if (Camera.current.tag == "MainCamera")
@@ -89,4 +144,9 @@ public class Platform : MonoBehaviour {
 
 		GL.PopMatrix ();
 	}
+
+    void startMoving()
+    {
+        start = true;
+    }
 }
