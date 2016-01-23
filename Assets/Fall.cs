d4626dc56b0b5c38755a09fe31e1dee02dd79faf
Mattias Aronsson <aronsson.mattias@gmail.com>
2016-01-24 00:34:32 +0100

    Fall

---
+++ Assets/Fall.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fall : MonoBehaviour {
+
+    private bool falling = false;
+    private float velocity = 0;
+    private float acceleration = 0.01f;
+    private float maxVelocity = 10;
+	
+	// Update is called once per frame
+	void Update () {
+        if (falling)
+        {
+            Transform rig = GetComponent<Transform>();
+            velocity += acceleration;
+            if(velocity >= maxVelocity)
+                velocity = maxVelocity;
+            Vector3 pos = rig.transform.position;
+            pos.y -= velocity;
+            rig.transform.position = pos;
+        }
+	}
+
+    public void StartFalling()
+    {
+        falling = true;
+    }
+
+}
