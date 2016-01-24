fb3408b003655d8ce1ba0d840407801fc5e26984
Rune Skovbo Johansen <rune@unity3d.com>
2016-01-24 14:14:15 +0100

    Sound on obstacles coming down.

--- Assets/Scripts/TriggerAnimation.cs
+++ Assets/Scripts/TriggerAnimation.cs
@@ -6,5 +6,9 @@ public class TriggerAnimation : MonoBehaviour {
 	void OnTriggerEnter (Collider other) {
 		GetComponent<Animation> ().Play();
 		GetComponent<BoxCollider> ().enabled = false;
+
+		AudioSource source = GetComponentInChildren<AudioSource> ();
+		if (source)
+			source.Play ();
 	}
 }
