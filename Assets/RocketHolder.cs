e816c12ba11bb54204242cd873a64e309bbe9ebf
oddruud <oddruud@gmail.com>
2016-01-24 14:28:32 +0100

    +Temple eyes activate

---
+++ Assets/RocketHolder.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using Rucrede;
+using UnityEngine.Events;
+
+public class RocketHolder : MonoBehaviour {
+
+	public Transform MovePosition;
+	public UnityEvent WhenRocketHolderInPlace;
+	
+	protected AudioSource _moveDownAudio;
+
+	// Use this for initialization
+	void Start () {
+		_moveDownAudio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void MoveDown() {
+		if (_moveDownAudio)
+			_moveDownAudio.Play();
+			
+		Tween.to(transform.position, MovePosition.position, 5.0f, Tween.EaseType.easeInOutSine,
+			(Tween t) => {transform.position = (Vector3) t.Value;},
+			(Tween t) => { RocketHolderInPlace();}
+		);
+	}
+	
+	protected void RocketHolderInPlace() {
+		//Rocket Holder in Place:
+		
+		if (WhenRocketHolderInPlace != null)
+			WhenRocketHolderInPlace.Invoke();
+	}
+	
+}
