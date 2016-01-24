7148e3528b15ac552938fc4f807786f3fdaea12f
oddruud <oddruud@gmail.com>
2016-01-24 15:06:54 +0100

    +rocketlock mechanism

---
+++ Assets/RocketLockMechanismController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class RocketLockMechanismController : MonoBehaviour {
+
+	public int Locks = 4;
+	public UnityEvent OnLocksRemoved;
+	
+	protected int _unlocked = 0;
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void LockRemoved() {
+		_unlocked++;
+		if (_unlocked == Locks) {
+			if (OnLocksRemoved != null)
+				OnLocksRemoved.Invoke();
+		}
+	}
+}
