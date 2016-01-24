4d404d7c78bb16be8b580661b094d1697bb1f780
oddruud <oddruud@gmail.com>
2016-01-24 16:32:19 +0100

    + rocket connected to lock mechanism

---
+++ Assets/Rocket.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rocket : MonoBehaviour {
+
+	public bool IsReadyForLaunch = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void ReadyForLaunch() {
+		IsReadyForLaunch = true;
+	}
+}
