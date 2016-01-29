f4551e25346324295bfeb1bc4801dd81de6e3c9e
oddruud <oddruud@gmail.com>
2016-01-29 11:24:56 +0100

    +screenshot utility

---
+++ Assets/Ruud/Editor/CaptureScreenshotEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using Rucrede.SwipeStory;
+
+[CustomEditor(typeof(CaptureScreenshot))]
+public class CaptureScreenshotEditor : Editor  {
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+		CaptureScreenshot capture = (CaptureScreenshot)target;
+		if (GUILayout.Button("Open screenshots folder")) {
+			capture.OpenScreenshotsFolder();
+		}
+	}
+}
---
+++ Assets/Ruud/ScreenShot/CaptureScreenshot.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using System.Diagnostics;
+using System;
+using System.Collections;
+
+public class CaptureScreenshot : MonoBehaviour {
+
+	public int superSizeFactor = 4;
+	public string Filename;
+	public bool DatePostFix = true;
+	public string Key = "s";
+
+	void LateUpdate () {
+		if (Input.GetKeyDown(Key)) {
+			string theFileName = Filename;
+			
+			if (DatePostFix)
+				theFileName = theFileName + "_" + System.DateTime.Now.ToLongTimeString();
+			
+			theFileName += ".png";
+			Application.CaptureScreenshot(Application.persistentDataPath + "/" + theFileName, superSizeFactor);
+			UnityEngine.Debug.Log("screenshot saved to:\n" + Application.persistentDataPath + "/" + theFileName);		
+		}
+	}
+	
+	[ContextMenu("open screenshots")]
+	public void OpenScreenshotsFolder() {
+	#if UNITY_EDITOR
+		Process p = new Process();
+		p.StartInfo.FileName = "open";
+		p.StartInfo.Arguments = ".";
+		p.StartInfo.WorkingDirectory =  Application.persistentDataPath;
+		p.StartInfo.UseShellExecute = true;
+		p.Start();
+	#endif
+	}
+	
+}
